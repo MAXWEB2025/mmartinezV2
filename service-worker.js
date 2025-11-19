ba00782afac2701aa3c92c4c3ef22226747748cb
Maxi <maxteel1989@gmail.com>
2025-11-18 23:38:04 -0300

    version1prueba

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", event => {
+    event.waitUntil(
+      caches.open("v1").then(cache => {
+        return cache.addAll([
+          "/",
+          "/index.html",
+          "/style.css",
+          "/main.js"
+        ]);
+      })
+    );
+  });
+  
